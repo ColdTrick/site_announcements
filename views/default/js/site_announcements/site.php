cf4351b4dfb5748ccc462e72b6d9c60a945c7aad
Jerome Bakker <jeabakker@coldtrick.com>
2016-01-15 14:30:42 +0100

    fixed: call to deprecated jQuery function

--- views/default/js/site_announcements/site.php
+++ views/default/js/site_announcements/site.php
@@ -2,24 +2,24 @@
 
 ?>
 //<script>
-elgg.provide("elgg.site_announcements");
+elgg.provide('elgg.site_announcements');
 
 elgg.site_announcements.init = function() {
-
-	$("#site-announcements-site li.elgg-menu-item-mark a").live("click", function(event) {
+	
+	$(document).on('click', '#site-announcements-site li.elgg-menu-item-mark a', function(event) {
 		event.preventDefault();
-
-		var guid = $(this).attr("rel");
 		
-		elgg.action($(this).attr("href"), {
+		var guid = $(this).attr('rel');
+		
+		elgg.action($(this).attr('href'), {
 			success: function(res) {
-				$("#elgg-object-" + guid).remove();
+				$('#elgg-object-' + guid).remove();
 			}
 		});
 	});
-
+	
 	elgg.ui.registerTogglableMenuItems('announcement-make-editor', 'announcement-remove-editor');
 }
 
-//register init hook
-elgg.register_hook_handler("init", "system", elgg.site_announcements.init);
+// register init hook
+elgg.register_hook_handler('init', 'system', elgg.site_announcements.init);
